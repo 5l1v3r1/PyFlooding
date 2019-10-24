ff582b7bfb627b2296fb2ca0c29df300313e0eff
RoughPWN <56966336+RoughPWN@users.noreply.github.com>
2019-10-24 15:31:28 +0200

    Source code of Flood

---
+++ PyFlooding.py
@@ -0,0 +1,39 @@
+import time 
+import pyautogui
+    
+//PyFlooder vers 1.2
+//Architectures tested ARMv7/x86/x64 
+//Based for Telegram Strangerbot Farm
+//Requirements:
+//pyautogui lib and Python 2.7 installed correctly.
+//Don't abuse this software for illegal actions.
+//Coded by RoughPWN
+//NoGUI only Shell executing.
+
+print("Waiting your position input...")
+print("Choose your position in 3 seconds")
+time.sleep(3)
+print(pyautogui.position())
+print("The position is right? Y or N")
+respond = input()
+if input(y);
+else input(n);
+    break
+while True:
+    pyautogui.click(pyautogui position())
+    pyautogui.write("/start")
+    pyautogui.hotkey("enter")
+    time.sleep(1)
+    pyautogui.write("your_text")
+    //You must write your text here.
+    pyautogui.hotkey("enter")
+    time.sleep(1)
+    pyautogui.write("/end")
+    pyautogui.hotkey("enter")
+    time.sleep(1)
+    print("Flood executes without errors")
+    print("Execution Flood: Infinite Loop")
+ 
+//Standard flooding without medias
+
+    
